eb72b886f6d9c1c103a76e4ab61735fb1d29126a
olahkrisz1 <olahkrisz1@gmail.com>
2023-07-24 20:58:29 +0200

    search bar added

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 'use client'
+import Image from 'next/image'
 import axios from "axios";
 import { useState } from "react";
 import { BsSearch } from 'react-icons/bs'
@@ -9,7 +10,7 @@ export default function Home() {
   const [weather, setWeather] = useState({}) 
   const [loading, setLoading] = useState(false)
 
-  const url=`https://api.openweathermap.org/data/2.5/weather?q=dubai&units=metric&appid=643ea4a08ab5b7c89bbc8d9ca131953e`
+  const url=`https://api.openweathermap.org/data/2.5/weather?q=dubai&units=metric&appid=${process.env.REACT_API_KEY}`
 
   const fetchWeather = (e) => {
     e.preventDefault()
@@ -24,10 +25,23 @@ export default function Home() {
 
   return (
     <div>
-    <main>
-      <h1>Hello world</h1>
-      <button onClick={fetchWeather}>Fetch data</button>
-    </main>
+      <div className='absolute top-0 left-0 bottom-0 bg-black/40 z-[1]' />
+      <Image 
+        src='https://cdn.pixabay.com/photo/2016/10/18/21/22/beach-1751455_1280.jpg' 
+        alt='weather'
+        fill={true}
+        className='object-cover'
+      />
+
+      <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10 '>
+        <form className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl '>
+          <div>
+            <input className='bg-transparent border-none text-white focus:outline-none text-2xl placeholder:text-white' type="text" placeholder='Search city'/>
+          </div>
+          <button onClick={fetchWeather}><BsSearch /></button>
+        </form>
+      </div>
+      
     </div>
   )
 }
